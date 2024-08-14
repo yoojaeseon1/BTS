6e01d87ce9b7f3e2eb8c3282985249d4208cfe54
choi <dacafo77@nate.com>
2024-08-14 16:11:28 +0900

    Feat:IntroduceVideoFragment, layout 생성

--- app/src/main/java/com/android/bts/presentation/detail/VideoDetailFragment.kt
+++ app/src/main/java/com/android/bts/presentation/detail/VideoDetailFragment.kt
@@ -7,21 +7,9 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.android.bts.R
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.app.IntroduceVideoFragment
 
 class VideoDetailFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,31 +17,32 @@ class VideoDetailFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_video_detail, container, false)
 
-        // 클릭 리스너 추가
         val textVideoIntro = view.findViewById<TextView>(R.id.text_video_intro)
         val textMemo = view.findViewById<TextView>(R.id.text_memo)
 
-        // 기본 선택 상태 설정
+        // 기본 선택 상태 설정 및 초기 프래그먼트 표시
         textVideoIntro.isSelected = true
         textMemo.isSelected = false
         updateBackgrounds(textVideoIntro, textMemo)
+        showFragment(IntroduceVideoFragment())
 
         textVideoIntro.setOnClickListener {
             textVideoIntro.isSelected = true
             textMemo.isSelected = false
             updateBackgrounds(textVideoIntro, textMemo)
+            showFragment(IntroduceVideoFragment())
         }
 
         textMemo.setOnClickListener {
             textVideoIntro.isSelected = false
             textMemo.isSelected = true
             updateBackgrounds(textVideoIntro, textMemo)
+
         }
 
         return view
     }
 
-    // 선택 상태에 따라 배경을 업데이트하는 메서드
     private fun updateBackgrounds(textVideoIntro: TextView, textMemo: TextView) {
         textVideoIntro.setBackgroundResource(
             if (textVideoIntro.isSelected) R.drawable.selected_background else R.drawable.default_background
@@ -63,14 +52,9 @@ class VideoDetailFragment : Fragment() {
         )
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            VideoDetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun showFragment(fragment: Fragment) {
+        childFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
