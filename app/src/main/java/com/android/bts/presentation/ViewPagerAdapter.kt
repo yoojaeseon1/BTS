855711071f9b7f06d5051a40f33d27125babeb86
choi <dacafo77@nate.com>
2024-08-19 03:09:07 +0900

    Feat:하단바 저장 클릭시 saved로이동 설정

--- app/src/main/java/com/android/bts/presentation/ViewPagerAdapter.kt
+++ app/src/main/java/com/android/bts/presentation/ViewPagerAdapter.kt
@@ -8,9 +8,10 @@ import com.android.bts.presentation.detail.VideoDetailFragment
 import com.android.bts.presentation.home.HomeFragment
 import com.android.bts.presentation.my.MyVideoFragment
 import com.android.bts.presentation.search.SearchFragment
+import com.example.app.save.SavedFragment
 
 class ViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
-    private val fragments = listOf(HomeFragment(), SearchFragment(), VideoDetailFragment(), MyVideoFragment())
+    private val fragments = listOf(HomeFragment(), SearchFragment(), SavedFragment(), MyVideoFragment())
 
     override fun getItemCount(): Int {
         return fragments.size
