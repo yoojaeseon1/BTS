cb7d077b8c7f9087134d8df9b3a13eb6a301db6c
hyehyunj <developh620@gmail.com>
2024-08-19 17:49:34 +0900

    feat: 추천프래그먼트 좌우버튼

--- app/src/main/java/com/android/bts/presentation/search/SearchRecommendFragment.kt
+++ app/src/main/java/com/android/bts/presentation/search/SearchRecommendFragment.kt
@@ -1,13 +1,16 @@
 package com.android.bts.presentation.search
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.android.bts.R
 import com.android.bts.databinding.FragmentSearchRecommendBinding
+import com.bumptech.glide.Glide
 
 class SearchRecommendFragment : Fragment(R.layout.fragment_search_recommend) {
     private var _binding: FragmentSearchRecommendBinding? = null
@@ -34,7 +37,108 @@ class SearchRecommendFragment : Fragment(R.layout.fragment_search_recommend) {
         super.onViewCreated(view, savedInstanceState)
 
 
+        //
+        sharedViewModel.recommendNumberData.observe(viewLifecycleOwner) {
+            when (it) {
+                recommendPlaceList.size -> sharedViewModel.setRecommendNumber(recommendPlaceList.size)
+                -1 -> sharedViewModel.setRecommendNumber(recommendPlaceList.size)
+                else -> recommendPlace(it)
+            }
+        }
 
+
+        //닫기버튼
+        binding.searchRecommendBtnExit.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.search_recommend_enter, R.anim.search_recommend_exit)
+                .remove(this).commit()
+        }
+        //왼쪽 버튼
+        binding.searchRecommendBtnLeft.setOnClickListener {
+            Log.d("클릭", "${animationSwit["view"]}")
+            animationSwit["button"] = "left"
+            sharedViewModel.nextRecommendNumber()
+
+        }
+        //오른쪽 버튼
+        binding.searchRecommendBtnRight.setOnClickListener {
+            animationSwit["button"] = "right"
+            sharedViewModel.previousRecommendNumber()
+
+        }
+    }
+
+
+    private fun recommendPlace(indexNumber: Int) {
+
+        val imageView = arrayOf(binding.searchRecommendIvFirst, binding.searchRecommendIvSecond)
+        val animation = arrayOf(
+            AnimationUtils.loadAnimation(requireContext(), R.anim.search_recommend_left_first),
+            AnimationUtils.loadAnimation(requireContext(), R.anim.search_recommend_left_second),
+            AnimationUtils.loadAnimation(requireContext(), R.anim.search_recommend_right_first),
+            AnimationUtils.loadAnimation(requireContext(), R.anim.search_recommend_right_second)
+        )
+        val recommendPlace = recommendPlaceList[indexNumber]
+
+        binding.apply {
+            searchRecommendTvSubTitle.text = recommendPlace.subTitle
+            searchRecommendTvTitle.text = recommendPlace.title
+            searchRecommendTvInfo.text = recommendPlace.info
+        }
+        when {
+            animationSwit["button"] == "left" -> {
+                imageView[0].startAnimation(animation[0])
+                imageView[1].startAnimation(animation[1])
+                when {
+                    animationSwit["view"] == "first" -> {
+                        Glide.with(requireContext())
+                            .load(recommendPlace.thumbnail.toUri())
+                            .timeout(6000)
+                            .into(imageView[1])
+                        animationSwit["view"] = "second"
+                    }
+                    animationSwit["view"] == "second" -> {
+                        Glide.with(requireContext())
+                            .load(recommendPlace.thumbnail.toUri())
+                            .timeout(6000)
+                            .into(imageView[0])
+                        animationSwit["view"] = "first"
+                    }
+                }
+            }
+
+            animationSwit["button"] == "right" -> {
+                imageView[0].startAnimation(animation[2])
+                imageView[1].startAnimation(animation[3])
+                when {
+                    animationSwit["view"] == "first" -> {
+                        Glide.with(requireContext())
+                            .load(recommendPlace.thumbnail.toUri())
+                            .timeout(6000)
+                            .into(imageView[0])
+                        animationSwit["view"] = "second"
+                    }
+                    animationSwit["view"] == "second" -> {
+                        Glide.with(requireContext())
+                            .load(recommendPlace.thumbnail.toUri())
+                            .timeout(6000)
+                            .into(imageView[1])
+                        animationSwit["view"] = "first"
+                    }
+                }
+
+
+
+            }
+        }
+
+        //마음에 들어요 버튼
+        binding.searchRecommendBtnAccept.setOnClickListener {
+            sharedViewModel.updateSearchWord(recommendPlace.searchWord)
+            parentFragmentManager.beginTransaction().remove(this).commit()
+        }
+
+    }
 
 
 
@@ -80,7 +184,7 @@ class SearchRecommendFragment : Fragment(R.layout.fragment_search_recommend) {
 //            }
 //
 //    })
-}
+
 
     override fun onDestroy() {
         super.onDestroy()
