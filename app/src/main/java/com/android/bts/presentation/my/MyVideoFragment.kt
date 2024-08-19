44b2c0de20d3528ff9af6cd0c2becf64c77e8075
Dyaoss <eogus12003@naver.com>
2024-08-19 14:19:57 +0900

    comment: 주석 잠깐 수정

--- app/src/main/java/com/android/bts/presentation/my/MyVideoFragment.kt
+++ app/src/main/java/com/android/bts/presentation/my/MyVideoFragment.kt
@@ -21,11 +21,6 @@ class MyVideoFragment : Fragment() {
     private var _binding: FragmentMyVideoBinding? = null
     private val binding get() = _binding!!
 
-    // 유저의 초기 로그인 데이터값을(로그인 프레그먼트에서 누른) 가져온다 -> 뷰모델 이용?
-    // 1. 데이터 클래스 구축
-    // 2. 데이터 텍스트뷰에 바인딩
-    // 3. 로그인 화면을 먼저 띄우기
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
